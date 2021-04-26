2af181b1aca80a3a810830eeb55b5aeb053042ed
Tikva1989 <81952636+Tikva1989@users.noreply.github.com>
2021-04-26 06:20:24 +0300

    commit

---
+++ week4/day1/dailyChallenge/dailyChallange_buildUpAString.py
@@ -0,0 +1,26 @@
+# Daily Challenge - Build Up A String
+
+# 1. ask the user for string 10 long characters
+# 2. Then, print the first and last characters of the given text.
+
+#
+# user_str = input("Enter a string")
+# # user_str_length = len(user_str)
+# #
+# if user_str_length < 10:
+#     print("string not long enough")
+# elif user_str_length > 10:
+#         print("string too long")
+# else:
+#         print(f'the first character is {user_str[0]} the last character is {user_str[(user_str_length-1)]}')
+
+# 3. Construct the string character by character: Print the first character,
+# then the second, then the third, until the full string is printed.
+
+# for x in user_str :
+#     print(x)
+
+# 4. . Swap some characters around then print the newly jumbled string (hint: look into the shuffle method)
+
+
+
